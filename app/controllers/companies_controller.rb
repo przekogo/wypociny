1407222db9b33578ec51375142c622d44c765811
adasko90 <adasko90@gmail.com>
2015-01-07 15:54:33 +0100

    Fixed user saving

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,8 +8,13 @@ class CompaniesController < ApplicationController
     user = User.new(email: params[:email], password: params[:password])
     user.role = "company"
     if user.save
-      company = Company.new(company_name: params[:company_name], company_description: params[:company_description], contact_name: params[:contact_name], contact_surname: params[:contact_surname], contact_telephone: params[:contact_telephone], company_address: params[:company_address], company_address2: params[:company_address2],  user_id: User.where(email: params[:email]).last.id)
-      company.save
+      company = Company.new(company_name: params[:company_name], company_description: params[:company_description], contact_name: params[:contact_name], contact_surname: params[:contact_surname], contact_telephone: params[:contact_telephone], company_address: params[:company_address], company_address2: params[:company_address2],  user_id: User.where(email: params[:email]).last.id)    
+      if company.save
+        user.company_id = company.id
+        user.save
+      else
+        user.destroy  #W razie błędu w zapisie do bazy usuwamy wszystko.
+      end
     end
     redirect_to new_user_session_path
   end
