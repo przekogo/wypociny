fc49f798b53b27f76e289e2f5b368a040c9876e2
Przemek Rogos <przeroog@gmail.com>
2015-01-10 13:31:18 +0100

    job adding

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,4 +5,20 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+  def new
+    @job = Job.new
+  end
+  def create
+    #tutej sie wpisze co czeba
+    job = Job.new(post_params)
+    job.company_id = current_user.company_id
+    if job.save #i bedzie
+      redirect_to company_path(current_user.company_id), notice: "dodano ofertę pracy"
+    end
+  end
+  
+  private
+  def post_params
+    params.require(:job).permit(:name)
+  end
 end
