509133b50f308b8a9c12ab7d49444a9ed330473a
Przemek Rogos <przeroog@gmail.com>
2015-01-10 12:18:26 +0100

    job views

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -11,7 +11,7 @@ class CompaniesController < ApplicationController
       company = Company.new(company_name: params[:company_name], company_description: params[:company_description], contact_name: params[:contact_name], contact_surname: params[:contact_surname], contact_telephone: params[:contact_telephone], company_address: params[:company_address], company_address2: params[:company_address2],  user_id: User.where(email: params[:email]).last.id)
       company.save
     end
-    redirect_to new_user_session_path, alert: "Lubisz placki!"
+    redirect_to new_user_session_path, notice: "Lubisz placki!"
   end
 
   def show
